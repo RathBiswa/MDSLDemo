140e0ca6e889158af0177b21468472548c8f9b11
RathBiswa <bbiswajit.rath@gmail.com>
2021-03-06 17:18:54 +0000

    updated runner class

--- src/test/java/runners/AverageAgeTest.java
+++ src/test/java/runners/AverageAgeTest.java
@@ -14,7 +14,10 @@ import java.io.File;
         features = "src/test/resources/AverageAge/AverageAge.feature"
         , glue = "src/test/java/step_definitions",
         plugin = {"pretty", "html:test-output", "json:json_output/cucumber.json"},
-        monochrome = true)
+        monochrome = true,
+        strict = true,
+        dryRun = false
+)
 public class AverageAgeTest {
     @AfterClass
     public static void writeExtentReport() {
